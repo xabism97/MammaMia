bcd4c85e152b9652f5f9b15b2fcdd4a689cc344f
AlvaroSuarezGomez <alvarosuarezgomez@opendeusto.es>
2022-11-12 13:18:49 +0100

    Añadidos modelos

---
+++ MammaMia/mammamiaapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class TipoMasa(models.Model):
+	nombre = models.CharField(max_length = 50)
+
+class Ingrediente(models.Model):
+	nombre = models.CharField(max_length = 50)
+
+class Pizza(models.Model):
+	nombre = models.CharField(max_length = 50)
+	tipoMasa = models.ForeignKey(TipoMasa, on_delete = models.CASCADE)
+	ingredientes = models.ForeignKey(Ingrediente, on_delete = models.CASCADE)
+	precio = models.FloatField()	
